4f54d1085de36c6b3339be1eed935b6339009e42
Shea Martin <shea@brewninja.net>
2025-09-02 11:43:23 -0300

    starting point

---
+++ prettier.config.ts
@@ -0,0 +1,9 @@
+export default {
+    trailingComma: 'es5',
+    tabWidth: 2,
+    semi: true,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+};
